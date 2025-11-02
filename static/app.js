d640685994689cc3e97c249b9f8e916106c6ad9f
dev_nick <developernikhil28@gmail.com>
2025-11-02 21:11:04 +0530

    fix: updated backend API URL for production

--- static/app.js
+++ static/app.js
@@ -40,7 +40,7 @@ async function handleCode(task, outputId, tabName) {
     showLoading(outputElement);
 
     try {
-        const response = await fetch("http://127.0.0.1:8000/process/", {
+        const response = await fetch("https://code-debugger-explainer-bot.onrender.com/process/", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ code: codeInput, task: task, language: language })
